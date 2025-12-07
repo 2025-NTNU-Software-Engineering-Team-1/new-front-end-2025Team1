819d5279ab6a7fd40cca5523155e94bf46802c1f
zylee0 <zylee0@gmail.com>
2025-12-07 19:07:13 +0800

    right login datas

--- tests/utils/arranges.ts
+++ tests/utils/arranges.ts
@@ -1,11 +1,25 @@
 import { expect, Page } from "@playwright/test";
 
-export async function login(page: Page) {
+export async function login_admin(page: Page) {
   await page.goto("/");
-  await page.getByPlaceholder("username or email").click();
-  await page.getByPlaceholder("username or email").fill("e2e-test");
-  await page.getByPlaceholder("password").click();
-  await page.getByPlaceholder("password").fill("tset-e2e");
+  await page.getByPlaceholder("username or email").fill("zylee");
+  await page.getByPlaceholder("password").fill("zylee33");
   await page.getByRole("button", { name: "Sign In" }).click();
-  await expect(page.locator(".card-title.mb-2").first()).toHaveText(/Welcome back, .*/);
+  await expect(page.locator(".card-title.mb-2").first()).toHaveText(/^Welcome back,/, { timeout: 5000 });
+}
+
+export async function login_teacher(page: Page) {
+  await page.goto("/");
+  await page.getByPlaceholder("username or email").fill("TeaK");
+  await page.getByPlaceholder("password").fill("1234");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page.locator(".card-title.mb-2").first()).toHaveText(/^Welcome back,/, { timeout: 5000 });
+}
+
+export async function login_stu(page: Page) {
+  await page.goto("/");
+  await page.getByPlaceholder("username or email").fill("stuA");
+  await page.getByPlaceholder("password").fill("1234");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page.locator(".card-title.mb-2").first()).toHaveText(/^Welcome back,/, { timeout: 5000 });
 }
